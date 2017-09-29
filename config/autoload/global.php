0e9adce2362483a040584062230285725f8c7fad
silla <silla@localhost>
2017-09-29 15:59:26 -0300

    Primeiro commit.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,28 @@
  * file.
  */
 
+use Zend\Session\Storage\SessionArrayStorage;
+use Zend\Session\Validator\RemoteAddr;
+use Zend\Session\Validator\HttpUserAgent;
+
 return [
-    // ...
+		// Session configuration.
+		'session_config' => [
+				// Session cookie will expire in 1 day.
+				'cookie_lifetime' => 60 * 60 * 24 * 1,
+				// Session data will be stored on server maximum for 30 days.
+				'gc_maxlifetime' => 60 * 60 * 24 * 30 
+		],
+		// Session manager configuration.
+		'session_manager' => [
+				// Session validators (used for security).
+				'validators' => [ 
+						RemoteAddr::class,
+						HttpUserAgent::class 
+				] 
+		],
+		// Session storage configuration.
+		'session_storage' => [ 
+				'type' => SessionArrayStorage::class 
+		]
 ];
