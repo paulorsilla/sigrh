3452f9c2a8092de2896d635948e3f5f5acd0dd3c
silla <silla@localhost>
2017-10-05 13:18:23 -0300

    Criação do arquivo Colaborador.php.

---
+++ module/SigRH/src/Entity/Colaborador.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SigRH\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Classe Colaborador.
+ * @ORM\Entity(repositoryClass="SigRH\Repository\Colaborador")
+ * @ORM\Table(name="colaborador")
+ */
+//class Colaborador extends AbstractEntity {
+//
+//}
