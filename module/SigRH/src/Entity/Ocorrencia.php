92b917d56800c296e2c8a3706256b0affdc1eb60
silla <silla@localhost>
2017-12-12 07:49:36 -0200

    Mantendo o filtro pelo campo ativo após editar um colaborador. Ajustes no controller da classe Ocorrencia a fim de buscar a escala do colaborador para comparações.

---
+++ module/SigRH/src/Entity/Ocorrencia.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SigRH\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Classe Ocorrencia.
+ * @ORM\Entity(repositoryClass="SigRH\Repository\Ocorrencia")
+ * @ORM\Table(name="ocorrencia")
+ */
+class Ocorrencia extends AbstractEntity {
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(name="data", type="datetime")
+     */
+    protected $dataOcorrencia; 
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Colaborador")
+     * @ORM\JoinColumn(name="colaborador_matricula", referencedColumnName="matricula")
+     * */
+    protected $colaboradorMatricula; //colaborador_matricula
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="BatidaPonto")
+     * @ORM\JoinColumn(name="batida_ponto_id", referencedColumnName="id")
+     * */
+    protected $batidaPonto;
+    
+    /**
+     * Returns user ID.
+     * 
+     * @return integer
+     */
+    public function getId() {
+        return $this->id;
+    }
+    function getColaboradorMatricula() {
+        return $this->colaboradorMatricula;
+    }
+    public function getDataOcorrencia() {
+        return $this->dataOcorrencia;
+    }
+    public function getBatidaPonto() {
+        return $this->batidaPonto;
+    }
+            
+    /**
+     * Sets user ID.
+     * 
+     * @param int $id        	
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+    function setColaboradorMatricula($colaboradorMatricula) {
+        $this->colaboradorMatricula = $colaboradorMatricula;
+    }
+    public function setDataOcorrencia($dataOcorrencia) {
+        $this->dataOcorrencia = $dataOcorrencia;
+    }
+    public function setBatidaPonto($batidaPonto) {
+        $this->batidaPonto = $batidaPonto;
+    }
+}
---
+++ module/SigRH/src/Repository/Ocorrencia.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SigRH\Repository;
+
+use SigRH\Entity\Ocorrencia as OcorrenciaEntity;
+
+class Ocorrencia extends AbstractRepository {
+
+    public function delete($id){
+        $row = $this->find($id);
+        if ($row) {
+                $this->getEntityManager()->remove($row);
+                $this->getEntityManager()->flush();
+        }
+    }
+    public function incluir_ou_editar($dados,$id = null){
+        
+        $row = null;
+        if ( !empty($id)) { // verifica se foi passado o codigo (se sim, considera edicao)
+            $row = $this->find($id); // busca o registro do campo para poder alterar
+        }    
+        if ( empty($row)) {
+            $row = new OcorrenciaEntity();
+        }
+        
+        $row->setData($dados); // setar os dados da model a partir dos dados capturados do formulario
+        $this->getEntityManager()->persist($row); // persiste o model no mando ( preparar o insert / update)
+        $this->getEntityManager()->flush(); // Confirma a atualizacao
+        
+        return $row;
+    }
+
+}
